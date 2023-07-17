982d685b0300bc0aa177403160ffb72e79e3645f
WebEngChild <WebEngChild@example.com>
2023-07-17 09:38:31 +0900

    feat: error handling

--- app/main.go
+++ app/main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"net/http"
-
 	"github.com/bradfitz/gomemcache/memcache"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
+	"net/http"
+	"os"
 )
 
 type Record struct {
@@ -20,7 +20,9 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
+	defer func(db *sql.DB) {
+		_ = db.Close()
+	}(db)
 
 	mc := memcache.New("localhost:11211")
 
@@ -29,39 +31,51 @@ func main() {
 	r.GET("/db", func(c *gin.Context) {
 		var result Record
 		// 3秒遅延させるロングランニングクエリの実行
-		err := db.QueryRow("SELECT *, SLEEP(3) FROM table WHERE id = ?", 1).Scan(&result.ID, &result.Value)
+		err := db.QueryRow("SELECT *, SLEEP(5) FROM table WHERE id = ?", 1).Scan(&result.ID, &result.Value)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
 		}
+
 		c.JSON(http.StatusOK, result)
 	})
 
 	r.GET("/cache", func(c *gin.Context) {
+		// キャッシュから取得
 		item, err := mc.Get("key")
+
+		// キャッシュがない場合
 		if err == memcache.ErrCacheMiss {
 			var result Record
+			// DBから取得
 			err = db.QueryRow("SELECT * FROM table WHERE id = ?", 1).Scan(&result.ID, &result.Value)
 			if err != nil {
 				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 				return
 			}
+			// キャッシュ登録
 			item = &memcache.Item{
 				Key:   "key",
 				Value: []byte(fmt.Sprintf("%d:%s", result.ID, result.Value)),
 			}
-			mc.Set(item)
+			if err := mc.Set(item); err != nil {
+				_, _ = fmt.Fprintln(os.Stderr, "Error closing the database: ", err)
+				return
+			}
 			c.JSON(http.StatusOK, result)
 			return
-		} else if err != nil {
+		}
+
+		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
 		}
 
+		// キャッシュ結果をレスポンス用に加工する
 		result := Record{}
-		fmt.Sscanf(string(item.Value), "%d:%s", &result.ID, &result.Value)
+		_, _ = fmt.Sscanf(string(item.Value), "%d:%s", &result.ID, &result.Value)
 		c.JSON(http.StatusOK, result)
 	})
 
-	r.Run(":8080")
+	_ = r.Run(":8080")
 }
